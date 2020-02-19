3ac7abedd4d231c7968286dc8339bea9ae132162
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-19 11:12:13 -0600

    set CF_APP to app name

--- scripts/build_cf.sh
+++ scripts/build_cf.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # set -x
 
-printenv
-
 # get manifest
 source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/asset_download.sh")
 ls -al
+export CF_APP=${APP_NAME}-monitored-cf

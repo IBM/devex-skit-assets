ae184e68a6278a7a0ad6ce39b454a99de7dd3182
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-19 13:04:52 -0600

    set CF app name before pushing

--- scripts/build_cf.sh
+++ scripts/build_cf.sh
@@ -4,4 +4,3 @@
 # get manifest
 source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/asset_download.sh")
 ls -al
-export CF_APP=${APP_NAME}-monitored-cf
--- scripts/deploy_cf.sh
+++ scripts/deploy_cf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Push app
+export CF_APP=$APP_NAME-monitored-cf
 if ! cf app "$CF_APP"; then  
   cf push "$CF_APP" -f ./manifest.yaml
 else

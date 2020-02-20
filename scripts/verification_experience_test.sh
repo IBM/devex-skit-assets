5766de5c03360ee0377407ae976015ea7879fbcf
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-19 20:45:31 -0600

    verification test script fixes

--- scripts/verification_experience_test.sh
+++ scripts/verification_experience_test.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-source build.properties
+exp_test_path=./scripts/experience_test.sh
+pd_evt_action=trigger
+pd_class="Skit Experience Test"
+pd_svc_name="DevX Skit Monitor"
+pd_severity=error
 
-cd scripts/
-if [ -f "experience_test.sh" ]; then
-  ./experience_test.sh
+if [ -f "$exp_test_path" ]; then
+  bash -c "$exp_test_path"
   if [ $? == 0 ]; then
-    pass_msg="Experience Test Passed"
+    pass_msg="Experience Test Passed :white_check_mark:"
     echo $pass_msg
-    ./slack_message.sh $pass_msg
+    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh" $pass_msg)
     exit 0
   else
     fail_msg="Experience Test Failed"
     echo $fail_msg
-    ./slack_message.sh $fail_msg
-    ./pagerduty_alert $fail_msg
+    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh" $fail_msg $pd_evt_action $pd_class $pd_svc_name $pd_severity)
+    fail_msg="$fail_msg :spinning-siren:"
+    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh" $fail_msg)
     exit 1
   fi
 else
-  msg="The 'experience_test.sh' script was not found."
+  msg="The '$exp_test_path' script was not found in the project's root folder. This script is required to pass verification."
   echo $msg
-  ./slack_message.sh $msg
-  ./pagerduty_alert.sh $msg 
+  source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh" $msg $pd_evt_action $pd_class $pd_svc_name $pd_severity)
+  fail_msg="$fail_msg :spinning-siren:"
+  source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh" $msg)
   exit 1
 fi

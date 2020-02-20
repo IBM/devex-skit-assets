71734a3bd4e31e7554883eafb069a27932db5200
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-19 21:03:03 -0600

    verification script fixes

--- scripts/verification_experience_test.sh
+++ scripts/verification_experience_test.sh
@@ -11,12 +11,12 @@ if [ -f "$exp_test_path" ]; then
   if [ $? == 0 ]; then
     pass_msg="Experience Test Passed :white_check_mark:"
     echo $pass_msg
-    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") $pass_msg
+    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") "$pass_msg"
     exit 0
   else
     fail_msg="Experience Test Failed"
     echo $fail_msg
-    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh") "$fail_msg" $pd_evt_action $pd_class $pd_svc_name $pd_severity
+    source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh") "$fail_msg" $pd_evt_action "$pd_class" "$pd_svc_name" $pd_severity
     fail_msg="$fail_msg :spinning-siren:"
     source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") "$fail_msg"
     exit 1
@@ -24,7 +24,7 @@ if [ -f "$exp_test_path" ]; then
 else
   msg="The '$exp_test_path' script was not found in the project's root folder. This script is required to pass verification."
   echo $msg
-  source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh") "$msg" $pd_evt_action $pd_class $pd_svc_name $pd_severity
+  source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh") "$msg" $pd_evt_action "$pd_class" "$pd_svc_name" $pd_severity
   fail_msg="$fail_msg :spinning-siren:"
   exit 1
   source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") "$msg"

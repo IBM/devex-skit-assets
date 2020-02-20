c6796e60bbf394ec2a3cba9c24c86fee3d3ffd0d
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-20 11:09:23 -0600

    chore: slack message tweaks

--- scripts/verification_experience_test.sh
+++ scripts/verification_experience_test.sh
@@ -14,23 +14,23 @@ if [ -f "$exp_test_path" ]; then
   cd ./scripts
   source "./$exp_test_script" || EXIT_CODE=$?
   if [ $EXIT_CODE == 0 ]; then
-    pass_msg="Experience Test Passed :white_check_mark:"
+    pass_msg="Skit Experience Test Passed :white_check_mark:"
     echo $pass_msg
     source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") "$pass_msg"
     exit 0
   else
-    fail_msg="Experience Test Failed"
+    fail_msg="Skit Experience Test Failed"
     echo $fail_msg
     source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh") "$fail_msg" "$pd_evt_action" "$pd_class" "$pd_svc_name" "$pd_severity"
-    fail_msg=":spinning-siren: $fail_msg"
+    fail_msg="$fail_msg :spinning-siren:"
     source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") "$fail_msg"
     exit 1
   fi
 else
-  msg="Experience Test script not found for skit $APP_NAME."
+  msg="Skit Experience Test script not found for skit $APP_NAME."
   echo $msg
   source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/pagerduty_alert.sh") "$msg" "$pd_evt_action" "$pd_class" "$pd_svc_name" "$pd_severity"
-  msg=":spinning-siren: $msg"
+  msg="$msg :spinning-siren:"
   source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/slack_message.sh") "$msg"
   exit 1
 fi

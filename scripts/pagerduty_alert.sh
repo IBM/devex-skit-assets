e89402e8d69aec8bd021d92a6475bc01abef02e4
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-19 20:57:52 -0600

    PD alert script fixes

--- scripts/pagerduty_alert.sh
+++ scripts/pagerduty_alert.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # set -x
 
-export ALERT_MSG=$1
+export ALERT_MSG="$1"
 export EVT_ACTION=$2
-export CLASS=$3
-export PD_SVC_NAME=$4
+export CLASS="$3"
+export PD_SVC_NAME="$4"
 export SEVERITY=$5
 
 export LOGS_URL="https://cloud.ibm.com/devops/pipelines/$PIPELINE_ID/$PIPELINE_STAGE_ID/$IDS_JOB_ID?env_id=ibm:yp:$IBM_CLOUD_REGION"

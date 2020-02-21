b46bfc4ed1544b8f8e9aaed0dd4e8b00ed2d41a2
Gabriel Valencia <gvalenc@us.ibm.com>
2020-02-21 15:19:21 -0600

    fix: typo

--- scripts/verification_experience_test.sh
+++ scripts/verification_experience_test.sh
@@ -15,7 +15,7 @@ fi
 if [ "$DEPLOY_TARGET" == "helm" ]; then
   source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/get_app_url_helm.sh")
 fi
-if [ "$DEPLOY_TARGET" == "knative"]; then
+if [ "$DEPLOY_TARGET" == "knative" ]; then
   source <(curl -sSL "$DEVX_SKIT_ASSETS_GIT_URL_RAW/master/scripts/get_app_url_knative.sh")
 fi
 echo "The APP_URL is: $APP_URL"

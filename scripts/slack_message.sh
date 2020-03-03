be28a5d0c8e5fa984e9d975ce0f0f903135e8ec2
Gabriel Valencia <gvalenc@us.ibm.com>
2020-03-03 11:09:05 -0600

    feat: updated slack msg format
    Signed-off-by: Gabriel Valencia <gvalenc@us.ibm.com>

--- scripts/slack_message.sh
+++ scripts/slack_message.sh
@@ -3,25 +3,17 @@
 # set -x
 
 export SLACK_MSG=$1
-export INCLUDE_LOGS=${2:-"true"}
+export NOTIFY=${2:-"true"}
 export IBM_CLOUD_REGION="${IBM_CLOUD_REGION:-us-south}"
 export LOGS_URL="https://cloud.ibm.com/devops/pipelines/$PIPELINE_ID/$PIPELINE_STAGE_ID/$IDS_JOB_ID?env_id=ibm:yp:$IBM_CLOUD_REGION"
-export TEXT="$IDS_JOB_NAME - $SLACK_MSG - Project: $IDS_PROJECT_NAME. Deployment Target: '"$DEPLOY_TARGET"'"
+export TEXT="$IDS_JOB_NAME - $SLACK_MSG - Project: $IDS_PROJECT_NAME. Deployment: '"$DEPLOY_TARGET"' || <'"$LOGS_URL"'|LOGS> || <'"$GIT_URL"'|REPO> || <'"$APP_URL"'|APP>"
 
-if [ "${INCLUDE_LOGS}" == "true" ]; then
+if [ "${NOTIFY}" == "true" ]; then
     curl -v POST --data-urlencode 'payload={"channel": "#'"$OWNER_SLACK_CHANNEL"'", 
                                     "username": "DevX Skit Monitor", 
-                                    "text": "@here '"$TEXT"'", 
+                                    "text": "'"$TEXT"' @here", 
                                     "link_names": "true",
-                                    "attachments": [
-                                        {
-                                            "pretext": "Visit the URL below to see the logs for this run.",
-                                            "author_name": "DevOps Pipelines",
-                                            "fallback": "Visit the pipeline to see logs for this run.",
-                                            "title": "LOGS",
-                                            "title_link": "'"$LOGS_URL"'"
-                                        }
-                                    ], "icon_emoji": ":police_car:"}' \
+                                    "attachments": [], "icon_emoji": ":police_car:"}' \
     $SLACK_WEBHOOK
 else
     curl -v POST --data-urlencode 'payload={"channel": "#'"$OWNER_SLACK_CHANNEL"'", 

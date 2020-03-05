32a71f55375cce71475e1904fceed4c6fabf99f0
Gabriel Valencia <gvalenc@us.ibm.com>
2020-03-05 14:12:29 -0600

    feat: add script versioning (#14)
    * feat: scripts versioning
    
    * fix: pass down assets git branch and version
    
    * fix: add versioning to script contents
    
    * fix: account for empty APP_URL
    
    * chore: minor comment edits

--- scripts/v1/deploy_helm.sh
+++ scripts/v1/deploy_helm.sh
@@ -3,12 +3,7 @@
 # set -x
 
 # copy the script below into your app code repo (e.g. ./scripts/check_and_deploy_helm.sh) and 'source' it from your pipeline job
-#    source ./scripts/check_and_deploy_helm.sh
-# alternatively, you can source it from online script:
-#    source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/check_and_deploy_helm.sh")
-# ------------------
-# source: https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/check_and_deploy_helm.sh
-
+#    source ./scripts/v1/check_and_deploy_helm.sh
 # This script checks the IBM Container Service cluster is ready, has a namespace configured with access to the private
 # image registry (using an IBM Cloud API Key), perform a Helm deploy of container image and check on outcome.
 source <(curl -sSL "https://raw.githubusercontent.com/open-toolchain/commons/master/scripts/check_and_deploy_helm.sh")
--- scripts/v1/slack_message.sh
+++ scripts/v1/slack_message.sh
@@ -6,7 +6,10 @@ export SLACK_MSG=$1
 export NOTIFY=${2:-"true"}
 export IBM_CLOUD_REGION="${IBM_CLOUD_REGION:-us-south}"
 export LOGS_URL="https://cloud.ibm.com/devops/pipelines/$PIPELINE_ID/$PIPELINE_STAGE_ID/$IDS_JOB_ID?env_id=ibm:yp:$IBM_CLOUD_REGION"
-export TEXT="$IDS_JOB_NAME - $SLACK_MSG - Project: $IDS_PROJECT_NAME. Deployment: "$DEPLOY_TARGET" || <"$LOGS_URL"|LOGS> || <"$GIT_URL"|REPO> || <"$APP_URL"|APP>"
+export TEXT="$IDS_JOB_NAME - $SLACK_MSG - Project: $IDS_PROJECT_NAME. Deployment: "$DEPLOY_TARGET" || <"$LOGS_URL"|LOGS> || <"$GIT_URL"|REPO>"
+if [[ -n $APP_URL ]]; then
+    export TEXT="$TEXT || <"$APP_URL"|APP>"
+fi
 
 if [ "${NOTIFY}" == "true" ]; then
     curl -v POST --data-urlencode 'payload={"channel": "#'"$OWNER_SLACK_CHANNEL"'", 

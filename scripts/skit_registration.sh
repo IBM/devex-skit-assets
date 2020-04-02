87d32bf2cb087e95eae2a5cdd8464d4d6153bc6f
Gabriel Valencia <gvalenc@us.ibm.com>
2020-04-02 10:12:20 -0500

    feat: support skit reg tekton pipeline

--- scripts/skit_registration.sh
+++ scripts/skit_registration.sh
@@ -2,8 +2,10 @@
 # uncomment to debug the script wherever it is used
 # set -x
 
-echo "Registering starter kit..."
-curl -X POST \
+OUT_FILE_RUN=run-pipeline-output.txt
+
+echo "Triggering starter kit registration..."
+curl -s -X POST \
   ${SKIT_REG_ENDPOINT} \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
@@ -17,5 +19,54 @@ curl -X POST \
     "url": "https://github.com/IBM/'${APP_NAME}'",
     "html_url": "https://github.com/IBM/'${APP_NAME}'",
     "statuses_url": "https://api.github.com/repos/IBM/'${APP_NAME}'/statuses/{sha}"
-  }
-}'
+  },
+  "skit_verified": "true"
+}' >> $OUT_FILE_RUN
+
+echo ""
+echo "Registration pipeline response:"
+cat $OUT_FILE_RUN
+echo ""
+echo ""
+
+PIPELINE_INFO_URL=$(cat $OUT_FILE_RUN | jq '.url')
+TEMP="${PIPELINE_INFO_URL%\"}"
+TEMP="${TEMP#\"}"
+PIPELINE_INFO_URL=${TEMP}
+echo "Pipeline info URL: $PIPELINE_INFO_URL"
+
+IAM_TOKEN=$(ibmcloud iam oauth-tokens --output json | jq '.iam_token')
+TEMP="${IAM_TOKEN%\"}"
+TEMP="${TEMP#\"}"
+IAM_TOKEN=${TEMP}
+
+echo ""
+echo "Checking pipeline run status..."
+SUCCESS="false"
+for ITERATION in {1..30}
+do
+    sleep 10
+
+    STATUS=$(curl -s -X GET -H "Authorization: $IAM_TOKEN" $PIPELINE_INFO_URL | jq '.status.state')
+    TEMP="${STATUS%\"}"
+    TEMP="${TEMP#\"}"
+    STATUS=${TEMP}
+    echo "Pipeline run status: $STATUS"
+
+    if [ "$STATUS" != "succeeded" ]; then
+        echo "Pipeline run is not finished, retrying after waiting..."
+    else
+        echo "Pipeline run is finished!"
+        SUCCESS="true"
+        break
+    fi
+done
+
+rm $OUT_FILE_RUN
+
+if [ "$SUCCESS" == "false" ]; then
+    echo "Registration failed. Check the registration pipeline logs for details."
+    exit 1
+else
+    echo "Skit registration succeeded!"
+fi

2d90dc69bc81ccc6dc7b30fc40d9d06c67b07ff8
Gabriel Valencia <gvalenc@us.ibm.com>
2021-05-13 10:00:35 -0500

    feat: tekton migration
    fix scripts path
    
    fix slack message
    
    fix pipeline run logs url
    
    remove unsupported slack msg portion
    
    add copy assets script
    
    fix dest dir
    
    dont set env vars here
    
    disable mention for now
    
    only set app url if necessary
    
    msg tweaks
    
    fix: skit url
    
    fix pipeline run url
    
    fix alert logs URL
    
    fix skit url during reg
    
    fix skit url
    
    fix python chart names
    
    use separate var

--- scripts/slack_message.sh
+++ scripts/slack_message.sh
@@ -5,12 +5,12 @@
 export SLACK_MSG=$1
 export NOTIFY=${2:-"true"}
 export IBM_CLOUD_REGION="${IBM_CLOUD_REGION:-us-south}"
-export LOGS_URL="https://cloud.ibm.com/devops/pipelines/$PIPELINE_ID/$PIPELINE_STAGE_ID/$IDS_JOB_ID?env_id=ibm:yp:$IBM_CLOUD_REGION"
-export LINKS="<"$LOGS_URL"|LOGS> || <"$GIT_URL"|REPO>"
+export LOGS_URL="$PIPELINE_RUN_URL"
+export LINKS="<"$LOGS_URL"|LOGS> || <"$SKIT_URL"|REPO>"
 if [[ -n $APP_URL ]]; then
     export LINKS="$LINKS || <"$APP_URL"|APP> "
 fi
-export TEXT="$LINKS - "$SLACK_MSG" \n\t*"$IDS_PROJECT_NAME"* on *"$DEPLOY_TARGET"* within *"$IDS_JOB_NAME"*"
+export TEXT="$LINKS - "$SLACK_MSG" \n\t*"${APP_NAME%"-monitored-cf"}"* on *"$DEPLOY_TARGET"*"
 
 
 if [ "${NOTIFY}" == "true" ]; then

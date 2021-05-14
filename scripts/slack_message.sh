1399a7b31f2690bddc59b11386599f85872d4577
Gabriel Valencia <gvalenc@us.ibm.com>
2021-05-14 15:28:25 -0500

    msg tweaks

--- scripts/slack_message.sh
+++ scripts/slack_message.sh
@@ -3,14 +3,14 @@
 # set -x
 
 export SLACK_MSG=$1
-export NOTIFY=${2:-"false"}
+export NOTIFY=${2:-"true"}
 export IBM_CLOUD_REGION="${IBM_CLOUD_REGION:-us-south}"
 export LOGS_URL="https://devops-api.$PIPELINE_REGION.devops.cloud.ibm.com/v1/tekton-pipelines/$PIPELINE_ID/runs/$PIPELINE_RUN_ID/$STEP_LOG_ID/logs"
 export LINKS="<"$LOGS_URL"|LOGS> || <"$SKIT_URL"|REPO>"
 if [[ -n $APP_URL ]]; then
     export LINKS="$LINKS || <"$APP_URL"|APP> "
 fi
-export TEXT="$LINKS - "$SLACK_MSG" \n\t*"$APP_NAME"* on *"$DEPLOY_TARGET"*"
+export TEXT="$LINKS - "$SLACK_MSG" \n\t*"${APP_NAME%"-monitored-cf"}"* on *"$DEPLOY_TARGET"*"
 
 
 if [ "${NOTIFY}" == "true" ]; then

3d09d4ff20c47bfd9d2ca66ccd0806351fbe610d
Gabriel Valencia <gvalenc@us.ibm.com>
2021-05-14 16:04:41 -0500

    fix pipeline run url

--- scripts/slack_message.sh
+++ scripts/slack_message.sh
@@ -5,7 +5,7 @@
 export SLACK_MSG=$1
 export NOTIFY=${2:-"true"}
 export IBM_CLOUD_REGION="${IBM_CLOUD_REGION:-us-south}"
-export LOGS_URL="https://devops-api.$PIPELINE_REGION.devops.cloud.ibm.com/v1/tekton-pipelines/$PIPELINE_ID/runs/$PIPELINE_RUN_ID/$STEP_LOG_ID/logs"
+export LOGS_URL="$PIPELINE_RUN_URL"
 export LINKS="<"$LOGS_URL"|LOGS> || <"$SKIT_URL"|REPO>"
 if [[ -n $APP_URL ]]; then
     export LINKS="$LINKS || <"$APP_URL"|APP> "

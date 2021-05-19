4b4b1e2a3505e496a5adc6c0b30b1c3d15b67515
Gabriel Valencia <gvalenc@us.ibm.com>
2021-05-19 13:23:01 -0500

    fix skit url during reg

--- scripts/skit_registration.sh
+++ scripts/skit_registration.sh
@@ -4,11 +4,10 @@
 
 function register_skit {
     OUT_FILE_RUN=run-pipeline-output.txt
-    export SKIT_NAME=(${GIT_URL##*/})
-    export SKIT_URL=${GIT_URL%%.git} # strips .git off the end
-    export SKIT_NAME=(${SKIT_NAME%%.*})
+    export SKIT_NAME=${APP_NAME%"-monitored-cf"}
+    export SKIT_URL="https://github.com/IBM/${SKIT_NAME}"
 
-    echo "Repository URL: $GIT_URL"
+    echo "Repository URL: $SKIT_URL"
     echo "Triggering starter kit registration for $SKIT_NAME"
     curl -s -X POST \
         ${SKIT_REG_ENDPOINT} \
